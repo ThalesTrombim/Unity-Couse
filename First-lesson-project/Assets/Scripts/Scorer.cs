9e84a776045afd2daf1ed7c62545c27f5916d5bc
Thales Trombim <thalestr@outlook.com>
2024-12-10 20:17:07 -0300

    feat: add conditions to collisions

--- First-lesson-project/Assets/Scripts/Scorer.cs
+++ First-lesson-project/Assets/Scripts/Scorer.cs
@@ -7,9 +7,10 @@ public class Scorer : MonoBehaviour
 
 	private void OnCollisionEnter(Collision other)
 	{
-		hits++;
-
-		Debug.Log("collision oject: " + other);
-		Debug.Log("You've bumped into a thing this many times: " + hits);
+		if(other.gameObject.tag != "Hit")
+		{
+			hits++;
+			Debug.Log("You've bumped into a thing this many times: " + hits);
+		}
 	}
 }
