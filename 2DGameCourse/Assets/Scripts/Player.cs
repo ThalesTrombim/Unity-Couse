5c92d8fc761b33063135ed0b5792eda634ac0630
Thales Trombim <thalestr@outlook.com>
2025-04-25 20:49:14 -0300

    feat: add idle and walking animation

--- 2DGameCourse/Assets/Scripts/Player.cs
+++ 2DGameCourse/Assets/Scripts/Player.cs
@@ -4,20 +4,28 @@ using UnityEngine;
 
 public class Player : MonoBehaviour
 {
-    public float speed;
-    private Rigidbody2D rig;
-    private Vector2 direction;
+	public float speed;
+	private Rigidbody2D rig;
+	private Vector2 _direction;
 
-    private void Start()
-    {
-        rig = GetComponent<Rigidbody2D>();
-    }
+	public Vector2 direction
+	{
+		get { return _direction; }
+		set { _direction = value; }
+	}
 
-    private void Update () {
-        direction = new Vector2(Input.GetAxisRaw("Horizontal"), Input.GetAxisRaw("Vertical"));
-    }
+	private void Start()
+	{
+		rig = GetComponent<Rigidbody2D>();
+	}
 
-    private void FixedUpdate() {
-        rig.MovePosition(rig.position + direction * speed * Time.fixedDeltaTime);
-    }
+	private void Update()
+	{
+		_direction = new Vector2(Input.GetAxisRaw("Horizontal"), Input.GetAxisRaw("Vertical"));
+	}
+
+	private void FixedUpdate()
+	{
+		rig.MovePosition(rig.position + direction * speed * Time.fixedDeltaTime);
+	}
 }
---
+++ 2DGameCourse/Assets/Scripts/PlayerMovement.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour
+{
+	private PlayerMovement player;
+
+	void Start()
+	{
+		player = GetComponent<Player>();
+	}
+
+	void Update()
+	{
+
+	}
+}
