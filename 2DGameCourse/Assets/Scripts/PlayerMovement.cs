67bd8f74fe20a99a481ecf95acdecab9dcdb513a
Thales Trombim <thalestr@outlook.com>
2025-04-27 16:50:01 -0300

    feat: add player movement animation

--- 2DGameCourse/Assets/Scripts/PlayerMovement.cs
+++ 2DGameCourse/Assets/Scripts/PlayerMovement.cs
@@ -4,15 +4,36 @@ using UnityEngine;
 
 public class PlayerMovement : MonoBehaviour
 {
-	private PlayerMovement player;
+	private Player player;
+	private Animator animator;
 
 	void Start()
 	{
 		player = GetComponent<Player>();
+		animator = GetComponent<Animator>();
 	}
 
 	void Update()
 	{
+		if (player.direction.sqrMagnitude > 0)
+		{
+			animator.SetInteger("transition", 1);
+		}
+		else
+		{
+			animator.SetInteger("transition", 0);
+		}
 
+		if (player.direction.x < 0)
+		{
+			transform.eulerAngles = new Vector2(0, 180);
+		}
+
+
+		if (player.direction.x > 0)
+		{
+			transform.eulerAngles = new Vector2(0, 0);
+		}
 	}
+
 }
